903f3f66d4dc349809b26a990cb2cf7dc2b016e3
Nassim Hafici <59237710+nassimhafici@users.noreply.github.com>
2023-06-05 00:47:29 -0400

    Update streamlit_app.py
    fix bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,13 @@ import streamlit as st
 import pandas as pd
 
 def get_fruit_list():
-  my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-  my_fruit_list = my_fruit_list.set_index("Fruit")
-  st.dataframe(my_fruit_list)
+  fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+  fruit_list = my_fruit_list.set_index("Fruit")
+  return fruit_list
+
+def get_multiselect_fruit_list(fruit_list):
+  preselected_fruits = preselected_fruits = ["Avocado", "Strawberries"]
+  st.multiselect("Pick some fruits:", list(fruit_list.index), preselected_fruits)
   
 def main() -> None:
   st.header('Breakfast Favorites')
@@ -14,9 +18,10 @@ def main() -> None:
   st.text('🥑🍞 Avocado Toast')
 
   st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-  preselected_fruits = ["Avocado", "Strawberries"]
-  st.multiselect("Pick some fruits:", list(my_fruit_list.index), preselected_fruits)
-  get_fruit_list()
+  
+  my_fruit_list = get_fruit_list()
+  get_multiselect_fruit_list(my_fruit_list)
+  st.dataframe(my_fruit_list)
   
 if __name__ == "__main__":
   main()
