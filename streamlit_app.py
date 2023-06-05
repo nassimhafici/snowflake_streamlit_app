3c410c4cfc322adfd9de7a4245d99ff50c3d31a4
Nassim Hafici <59237710+nassimhafici@users.noreply.github.com>
2023-06-05 00:45:09 -0400

    Update streamlit_app.py
    add table
    add multiselect widget

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,22 @@
-import streamlit
-streamlit.header('Breakfast Favorites')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
+import streamlit as st
+import pandas as pd
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+def get_fruit_list():
+  my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+  my_fruit_list = my_fruit_list.set_index("Fruit")
+  st.dataframe(my_fruit_list)
+  
+def main() -> None:
+  st.header('Breakfast Favorites')
+  st.text('🥣 Omega 3 & Blueberry Oatmeal')
+  st.text('🥗 Kale, Spinach & Rocket Smoothie')
+  st.text('🐔 Hard-Boiled Free-Range Egg')
+  st.text('🥑🍞 Avocado Toast')
 
+  st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+  preselected_fruits = ["Avocado", "Strawberries"]
+  st.multiselect("Pick some fruits:", list(my_fruit_list.index), preselected_fruits)
+  get_fruit_list()
+  
+if __name__ == "__main__":
+  main()
